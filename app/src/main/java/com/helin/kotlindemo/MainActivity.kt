25e1d33619a98a3d46c4bcd2ca9c75b163f81734
helin <991293917@qq.com>
2018-04-04 14:18:29 +0800

    some commit

--- app/src/main/java/com/helin/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/helin/kotlindemo/MainActivity.kt
@@ -23,8 +23,7 @@ class MainActivity : BaseActivity() {
 
     val mFragments: Array<Fragment> = arrayOf(TextFragment(), PicFragment(), GifFragment())
 
-    val mPre:Int =1
-    val mPrexsfs: Int = 198
+    val mPrexsfs: String = "zhufenzhi"
 
     var lastTime :Int by Preference(this@MainActivity, "sp_key_default_fragment", 0)
 
